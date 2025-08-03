5d8c492822b61e4900bbf2e01b09526f60703151
Kajal Rawal <kajalrawal1358@gmail.com>
2025-08-03 21:10:40 +0530

    Time: 2 ms (57.57%) | Memory: 44.2 MB (78.51%) - LeetSync

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,20 @@
+class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    int idx;
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        for(int i = 0; i < inorder.length; i++) map.put(inorder[i], i);
+        idx = postorder.length - 1;
+        return build(postorder, inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] postorder, int[] inorder, int s, int e) {
+        if(s > e) return null;
+        int curr = postorder[idx--];
+        TreeNode root = new TreeNode(curr);
+        int id = map.get(curr);
+        root.right = build(postorder, inorder, id + 1, e);
+        root.left = build(postorder, inorder, s, id - 1);
+        return root;
+    }
+}
